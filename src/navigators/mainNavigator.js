f6ac9f9e87ace596d2b6d47c2a2747dd38ace4ee
dani-kjh <daniescribanoperez@gmail.com>
2022-03-22 16:49:50 +0100

    Añadidos iconos a la barra de navegacion inferior

--- src/navigators/mainNavigator.js
+++ src/navigators/mainNavigator.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeScreen, SettingsScreen, ProfileScreen } from '../pages';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import i18n from 'i18n-js';
 
 const Tab = createBottomTabNavigator();
@@ -8,7 +9,23 @@ function MainNavigator(){
 
 
     return(
-        <Tab.Navigator initialRouteName="Map">
+        <Tab.Navigator initialRouteName="Map"
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({  color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Map') {
+                iconName = 'map';
+              } else if (route.name === 'Settings') {
+                iconName = 'settings';
+              }
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
             <Tab.Screen 
                 name="Map"
                 component={HomeScreen} 
