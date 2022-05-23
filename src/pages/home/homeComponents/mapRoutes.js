9bec446793b40db79e0626a6848fc13387e37cd9
theAlberkut <alberto.martinez.berlanga@estudiantat.upc.edu>
2022-05-23 15:16:55 +0200

    hola daniel

--- src/pages/home/homeComponents/mapRoutes.js
+++ src/pages/home/homeComponents/mapRoutes.js
@@ -1,16 +1,45 @@
 import MapViewDirections from 'react-native-maps-directions';
-import React, {useState, useEffect} from "react";
+import React, { Component, useEffect, useState, useRef, useImperativeHandle, forwardRef } from 'react';
+import { StyleSheet, ActivityIndicator, View, Image, TouchableOpacity } from 'react-native';
 import {API_KEY} from '@env';
+import useCloseStation from "../../../hooks/useCloseStation";
+
+
+
 export default ({routeActivate, location, ChangeRoutingInfo}) => {
     const destination = {latitude:routeActivate.latitude, longitude:routeActivate.longitude};
     const currentTransport = routeActivate.objectType == "vehicleStation" ? "DRIVING" : "BICYCLING";
     const [lineStyle, setLineStyle] = useState({width: 3, dash:null, mode:"DRIVING", color: "red"})
-    
-    
+    let distancia = routeActivate.distancia;
+    let autonomia =1;
+    const isLoading = false;
+
+    const closet={latitude:41.420061, longitude:2.201705}
+ 
     useEffect(() =>{       
         changeLine(routeActivate)
     }, [routeActivate])
-
+    
+    const {getCloserStation} = useCloseStation();
+    const [estation, setStation] = useState([]);
+   
+    useEffect(async () => {
+        getStation()
+      }, []);
+      
+    
+      const getStation = async () => {
+            let stacion = await getCloserStation(location.latitude, location.longitude,1) ;
+            if(stacion != null && stacion != undefined){
+                setStation(stacion)
+            }
+           
+      }
+        console.log(estation)
+      //console.log(estation?.nearest[0]?.lat)
+    // const closet={latitude:estation?.nearest[0]?.lat, longitude:estation?.nearest[0]?.lng}
+    
+   
     
     const changeLine = (userPreferences) =>{
         if (userPreferences !== null){
@@ -29,8 +58,13 @@ export default ({routeActivate, location, ChangeRoutingInfo}) => {
             }
         }
     }
+
+
     return(
+
        
+          <View>
+              { distancia < autonomia ?
             <MapViewDirections
             origin={location}
             destination={destination}
@@ -46,7 +80,27 @@ export default ({routeActivate, location, ChangeRoutingInfo}) => {
                 })
             }
             />
-       
-    )
-    
+            :  <MapViewDirections
+            origin={location}
+            destination={destination}
+            apikey={API_KEY}
+            waypoints={[closet]}
+            splitWaypoints={true}
+            optimizeWaypoints={true}
+            stopover={true}
+            mode={lineStyle.mode}
+            strokeWidth={lineStyle.width}
+            strokeColor={lineStyle.color}
+            lineDashPattern={lineStyle.dash}
+            timePrecision={"now"}
+            onReady={result => ChangeRoutingInfo({
+                distance:result.distance, 
+                duration:result.duration
+                })
+            }
+            />
+        }
+     </View>
+    );
+
 }
