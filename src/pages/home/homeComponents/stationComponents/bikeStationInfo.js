d64c86d104d1eddd81be1390444a600b93d5ff48
AwaKT <gerardm27@gmail.com>
2022-05-26 19:38:29 +0200

    wip bike info

--- src/pages/home/homeComponents/stationComponents/bikeStationInfo.js
+++ src/pages/home/homeComponents/stationComponents/bikeStationInfo.js
@@ -1,73 +1,68 @@
 import React from "react";
 import i18n from "i18n-js";
 import { StyleSheet, Pressable, View, Image, Text } from "react-native";
+import CustomProgressBar from "../../../../utils/customProgressBar";
 
 function BikeStationInfo(props) {
+  const customStyle = require("../../../../utils/customStyleSheet");
+
   return (
-    <View style={styles.bikeStationContent}>
-      <View style={styles.bikeContent}>
-        <View style={styles.bikeSlotType}>
-          <Image
-            source={require("../../../../../assets/images/icons/mechanical.png")}
-            style={styles.vehicleIcon}
-          />
-          <Text style={styles.stationBikeText}>
-            {props.stationInfo.data.sockets[0].available_mechanical}{" "}
-            {i18n.t("locationInfo.mechanical")}
-          </Text>
+    <View style={[styles.bikeStationContent, {backgroundColor: "blue"}]}>
+      <View style={[styles.bikeInfo, {backgroundColor: "red"}]}>
+        <View style={[styles.bikeInfoLeft, {backgroundColor: "yellow"}]}>
+          <View style={{width: "45%"}}>
+            <Image
+              source={require("../../../../../assets/images/icons/mechanical.png")}
+              style={styles.vehicleIcon}
+            />
+          </View>
         </View>
-        <View style={styles.bikeSlotType}>
-          <Image
-            source={require("../../../../../assets/images/icons/electrical.png")}
-            style={styles.vehicleIcon}
-          />
-          <Text style={styles.stationBikeText}>
-            {props.stationInfo.data.sockets[0].available_electrical}{" "}
-            {i18n.t("locationInfo.electrical")}
-          </Text>
-        </View>
-        <View style={styles.bikeSlotType}>
-          <Image
-            source={require("../../../../../assets/images/icons/parking.png")}
-            style={styles.vehicleIcon}
-          />
-          <Text style={styles.stationBikeText}>
-            {props.stationInfo.data.sockets[0].available_sockets}{" "}
-            {i18n.t("locationInfo.parking")}
-          </Text>
+        <View style={styles.bikeInfoRight}>
+
         </View>
       </View>
+      <View style={[styles.parkingInfo, {backgroundColor: "green"}]}>
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   bikeStationContent: {
-    display: "flex",
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  bikeInfo: {
+    height: "50%",
+    width: "100%",
     flexDirection: "row",
     justifyContent: "space-between",
-    maxWidth: "100%",
-  },
-  bikeSlotType: {
     alignItems: "center",
   },
-  bikeContent: {
+  bikeInfoLeft: {
+    width: "50%",
     flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bikeInfoRight: {
+    width: "50%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  parkingInfo: {
+    height: "50%",
     width: "100%",
-    alignSelf: "center",
-    justifyContent: "space-between",
   },
   vehicleIcon: {
     height: 40,
     width: 40,
     marginTop: 10,
     marginLeft: 5,
-  },
-  stationBikeText: {
-    marginTop: 10,
-  },
-  favouriteButton: {
-    marginLeft: "auto",
   },
 });
 
